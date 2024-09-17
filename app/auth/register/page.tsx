367d920343b838da7ebfe1cef176122020313259
Mohammad Alsallal <malslal787@gmail.com>
2024-09-17 11:41:53 +0300

    register form bug fixes

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -2,7 +2,7 @@ import { RegisterForm } from "@/components/auth/register-form";
 
 const page = () => {
   return (
-    <div className="relative w-full h-fit flex items-center justify-center bg-black bg-transparent bg-[url('/petra.jpg')] bg-no-repeat bg-cover bg-center">
+    <div className="relative w-full min-h-screen h-fit flex items-center justify-center bg-black bg-transparent bg-[url('/petra.jpg')] bg-no-repeat bg-cover bg-center">
       <div className="absolute inset-0 bg-gradient-to-t from-black/85 via-black/50 to-transparent"></div>
 
       <div className="relative z-10 my-10">
